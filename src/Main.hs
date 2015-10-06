1a88a1bf1544b1cb0d681ec26b8c9ac1ac76fdec
Hans-Peter Deifel <hpd@hpdeifel.de>
2015-10-06 11:46:51 +0200

    Implement better date parsing

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, LambdaCase #-}
+
 module Main where
 
 import           Brick
@@ -7,11 +8,13 @@ import           Brick.Widgets.Border
 
 import           Control.Lens
 import           Control.Monad
+import           Control.Monad.IO.Class
 import           Data.Default
+import           Data.Monoid
 import           Data.Text (Text)
 import qualified Data.Text as T
 import           Data.Text.Zipper
-import           Data.Time
+import           Data.Time hiding (parseTime)
 import           Graphics.Vty.Input.Events
 import qualified Hledger as HL
 
@@ -22,6 +25,7 @@ data AppState = AppState
 
 data WizardState = DateQuestion
                  | DescriptionQuestion Day
+                 | AccountQuestion Day Text [HL.Posting]
 
 draw :: AppState -> [Widget]
 draw as = [ui]
@@ -34,32 +38,51 @@ draw as = [ui]
 event :: AppState -> Event -> EventM (Next AppState)
 event as ev = case ev of
   EvKey KEsc [] -> halt as
-  EvKey KEnter [] -> continue (nextStep as)
+  EvKey KEnter [] -> liftIO (nextStep as) >>= continue
   _ -> (AppState <$> handleEvent ev (asEditor as) <*> return (asWizardState as)) >>= continue
 
 renderState :: WizardState -> Widget
 renderState DateQuestion = txt " "
 renderState (DescriptionQuestion date) = txt $ mconcat
-  [ T.pack $ formatTime defaultTimeLocale "%d.%m.%Y" date
+  [ "Date: " <> (T.pack $ formatTime defaultTimeLocale "%d.%m.%Y" date) ]
+renderState (AccountQuestion date desc postings) = txt $ mconcat
+  [ "Date: " <> (T.pack $ formatTime defaultTimeLocale "%d.%m.%Y" date)
+  , "Description: " <> desc
   ]
 
 renderQuestion :: WizardState -> Widget
 renderQuestion DateQuestion = txt "Date: "
 renderQuestion (DescriptionQuestion _) = txt "Description: "
+renderQuestion (AccountQuestion _ _ ls) = txt $ "Account" <> (T.pack $ show $ length ls) <> ": "
 
-nextStep :: AppState -> AppState
+nextStep :: AppState -> IO AppState
 nextStep as = case asWizardState as of
-  DateQuestion -> as { asWizardState = DescriptionQuestion (T.pack $ concat $ getEditContents $ asEditor as)
-                     , asEditor = asEditor as & editContentsL .~ stringZipper [""] (Just 1)
-                     }
-  (DescriptionQuestion _) -> as
+  DateQuestion -> do
+    parseTime (editText as) >>= \case
+      Nothing -> return as -- TODO Show error
+      Just day -> return $ as
+        { asWizardState = DescriptionQuestion day
+        , asEditor = asEditor as & editContentsL .~ stringZipper [""] (Just 1)
+        }
+  (DescriptionQuestion d) -> return as
+    { asWizardState = AccountQuestion d (editText as) []
+    , asEditor = asEditor as & editContentsL .~ stringZipper [""] (Just 1)
+    }
 
-parseTime :: Text -> Maybe Text
-parseTime t = case T.splitOn "." t of
-  [d] -> Just _
-  [d,m] -> Just _
-  [d,m,y] -> Just _
-  _ -> Nothing
+editText = T.pack . concat . getEditContents . asEditor
+
+parseTime :: Text -> IO (Maybe Day)
+parseTime t = do
+  (currentYear, currentMonth, currentDay) <- toGregorian . utctDay <$> getCurrentTime
+  case filter (not . T.null) $ T.splitOn "." t of
+    [d] -> return $ Just $ fromGregorian currentYear currentMonth (parseInt d)
+    [d,m] -> return $ Just $ fromGregorian currentYear (parseInt m) (parseInt d)
+    [d,m,y] -> return $ Just $ fromGregorian (parseInt y) (parseInt m) (parseInt d)
+    _ -> return Nothing
+
+-- TODO: Handle failure
+parseInt :: (Read a, Num a) => Text -> a
+parseInt t = read $ T.unpack t
 
 main :: IO ()
 main =
