feb5ce5edb2ae3858c35fe5ed2267d90ccdd6575
Hans-Peter Deifel <hpd@hpdeifel.de>
2017-08-08 18:55:25 +0200

    Fix build against hledger-lib HEAD

--- src/AmountParser.hs
+++ src/AmountParser.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE CPP #-}
+
 module AmountParser (parseAmount) where
 
 import           Data.Text (Text)
@@ -6,7 +8,11 @@ import           Data.Functor.Identity
 import           Control.Monad.Trans.State.Strict
 import           Text.Megaparsec.Compat hiding (Parser)
 
+#if MIN_VERSION_hledger_lib(1,3,99)
+type Parser a = HL.JournalParser Identity a
+#else
 type Parser a = HL.JournalStateParser Identity a
+#endif
 
 parseAmount :: HL.Journal -> Text -> Either String HL.MixedAmount
 parseAmount journal t = case runIdentity $ runParserT (evalStateT (mixed <* optional space <* eof) journal) "" t of
